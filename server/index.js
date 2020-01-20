c5dffd6653916eaa96b64713ab97d1e87f9b1f97
Bojan Jovanovic <bojanjovanovic@Bojans-MacBook-Pro-2.local>
2020-01-19 20:49:22 -0500

    adding packages

---
+++ server/index.js
@@ -0,0 +1,22 @@
+// import express from 'express';
+// import morgan from 'morgan';
+// import bodyParser from 'body-parser';
+
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+
+const app = express();
+const PORT = 8080;
+
+app.use(morgan('dev'));
+
+// body parsing middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
+
+app.listen(PORT, () => {
+  console.log(`Listening on PORT ${PORT}`);
+});
